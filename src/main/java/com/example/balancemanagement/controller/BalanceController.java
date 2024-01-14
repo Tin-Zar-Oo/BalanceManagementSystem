7de3de634b6c828bec01b9769607bc68fef1cace
Tin-Zar-Oo <nangtinzaroo6796@gmail.com>
2024-01-14 22:51:12 +0630

    added balance details

--- src/main/java/com/example/balancemanagement/controller/BalanceController.java
+++ src/main/java/com/example/balancemanagement/controller/BalanceController.java
@@ -3,9 +3,14 @@ package com.example.balancemanagement.controller;
 
 
 import com.example.balancemanagement.model.entity.Balance;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
-import java.util.*;
+@Controller
+@RequestMapping("user/balance")
 public class BalanceController {
 
     public String addNew() {
@@ -21,9 +26,10 @@ public class BalanceController {
         return "";
     }
 
-    public String findById(int id) {
-
-        return "";
+    @GetMapping("{id:\\d+}")
+    public String findById(@PathVariable int id) {
+        System.out.println("Balance Id : %d".formatted(id));
+        return "balance-details";
     }
 
     public String search(Balance.Type type, String category, LocalDate from, LocalDate to) {
